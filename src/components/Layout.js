560b77985ba07e3e30a0fae06cfa833e69c03846
David Agbaeze <you@example.com>
2022-07-25 15:14:01 +0100

    code upload

---
+++ src/components/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Head from 'next/head'
+import Header from './Header'
+
+export default function Layout({title, children, keywords, description}) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='keywords' content={keywords}/>
+        <metq name='description' content={description}/>
+        <link rel='icon' href='/favicon.ico'/>
+      </Head>
+
+      <Header/>
+
+      <main className='container mx-auto my-7'>{children}</main>
+    </div>
+  )
+}
+
+
+Layout.defaultProps = {
+  title : 'DevSpace Blog',
+  keywords: 'deveopment, coding, programming',
+  description: 'The best info and news on development'
+}
---
+++ src/pages/404.js
@@ -0,0 +1,18 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+
+export default function NotfoundPage() {
+  return (
+    <Layout title='Page Not Found'>
+      <div className='flex flex-col items-center mt-20'>
+        <Image src='/images/logo.png' width={70} height={70} className='bg-gray-800 rounded-2xl' />
+
+        <h1 className='text-6xl my-5'>Whoops!</h1>
+        <h2 className='text-4xl text-gay-400 mb-5'>This page doesn't exist</h2>
+      </div>
+    </Layout>
+  )
+}
