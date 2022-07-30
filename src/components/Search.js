da470ecd1d618f689d750ee89f77fc1885fdb2b7
David Agbaeze <you@example.com>
2022-07-30 21:26:22 +0100

    new updates

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,24 @@
-import React from 'react'
 import { useState, useEffect } from 'react'
-import {FaSearch} from 'react-icons/fa'
+import { FaSearch } from 'react-icons/fa'
+import SearchResults from './SearchResults'
 
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState('')
-  const [searchResults, setSearchResults] =useState([])
+  const [searchResults, setSearchResults] = useState([])
 
-  useEffect(() =>{
+  useEffect(() => {
     const getResults = async () => {
-      if(searchTerm ===''){
+      if (searchTerm === '') {
         setSearchResults([])
-      } else{
-        const res = await fetch(`api/search?q=T${searchTerm}`)
-        const {results} = await res.json()
-        setSearchResults(results)
+      } else {
+        const res = await fetch(`/api/search?q=${searchTerm}`)
+        const { results } = await res.json()
+        setSearchResults(results.frontmatter)
       }
     }
 
     getResults()
-  },[searchTerm])
+  }, [searchTerm])
 
   return (
     <div className='relative bg-gray-600 p-4'>
@@ -26,19 +26,21 @@ export default function Search() {
         <div className='relative text-gray-600 w-72'>
           <form>
             <input
-            type='search' 
-            name='search' 
-            id='search'
-            className='bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none w-72' 
-            value={searchTerm}
-            onChange={(e)=> setSearchTerm(e.target.value)}
-            placeholder='Search Posts...'
+              type='search'
+              name='search'
+              id='search'
+              className='bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none w-72'
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder='Search Posts...'
             />
-            <FaSearch className='absolute top-0 right-0 text-black mt-3 mr-4'/>
+
+            <FaSearch className='absolute top-0 right-0 text-black mt-3 mr-4' />
           </form>
         </div>
       </div>
+
+      <SearchResults results={searchResults} />
     </div>
   )
 }
-
--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -3,29 +3,38 @@ import path from 'path'
 import matter from 'gray-matter'
 
 
-export default function handler(req, res) {
+export default (req, res) => {
   let posts
-  
-  if(process.env.NODE_ENV=== 'production'){
-    //
-  }else {
-    const files = fs.readdirSync(path.join('src/posts'))
-    posts = files.map(filename =>{
-      const markdownWithMeta = fs.readFileSync(path.join('src/posts', filename), 'utf-8')
-      const {data: frontmatter} = matter(markdownWithMeta)
 
-      return{
-        frontmatter
+  if (process.env.NODE_ENV === 'production') {
+    // Fetch from cache
+    posts = require('@/cache/data').posts
+  } else {
+    const files = fs.readdirSync(path.join('./src/posts'))
+
+    posts = files.map((filename) => {
+      const slug = filename.replace('.md', '')
+
+      const markdownWithMeta = fs.readFileSync(
+        path.join('./src/posts', filename),
+        'utf-8'
+      )
+
+      const { data: frontmatter } = matter(markdownWithMeta)
+
+      return {
+        slug,
+        frontmatter,
       }
     })
-
   }
 
-  const results = posts.filter(({frontmatter:{title, category,excerpt}})=>
-    title.toLowerCase().indexOf(req.query.q) != -1 ||
-    category.toLowerCase().indexOf(req.query.q) != -1 ||
-    excerpt.toLowerCase().indexOf(req.query.q) != -1 
+  const results = posts.filter(
+    ({ frontmatter: { title, excerpt, category } }) =>
+      title.toLowerCase().indexOf(req.query.q) != -1 ||
+      excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
+      category.toLowerCase().indexOf(req.query.q) != -1
   )
-  
-  res.status(200).json(JSON.stringify({results}))
+
+  res.status(200).json(JSON.stringify({ results }))
 }
