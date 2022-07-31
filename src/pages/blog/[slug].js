435b9eecf3699563df059b63f2e5fc5deacf29a2
David Agbaeze <you@example.com>
2022-07-31 07:28:31 +0100

    ..

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -16,10 +16,10 @@ export default function PostPage({frontmatter: {title, category, date, author_im
           <h1 className='text-5xl mb-7'>{title}</h1>
           <CategoryLabel>{category}</CategoryLabel>
         </div>
-        <img src={cover_image} alt='' className='w-full rounded' />
+        <Image src={cover_image} alt='' className='w-full rounded' />
         <div className='flex justify-between items-center bg-gray-100 p-2 my-8'>
           <div className='flex items-center'>
-            <img src={author_image} alt='' className='mx-4 w-10 h-10 object-cover rounded-full hidden sm:block'/>
+            <Image src={author_image} alt='' className='mx-4 w-10 h-10 object-cover rounded-full hidden sm:block'/>
             <h4>{author}</h4>  
           </div>
           <div className='mr-4'>{date}</div>
